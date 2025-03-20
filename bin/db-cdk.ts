1bd47dd8e1203450ac7f3112cb5e9e4692e95a48
alwin <alwinseb01@gmail.com>
2025-03-21 04:34:58 +0530

    aws account and region config

--- bin/db-cdk.ts
+++ bin/db-cdk.ts
@@ -15,6 +15,9 @@ new DbCdkStack(app, 'DbCdkStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
+  env: {
+    account: '209479268297',
+    region: 'ca-central-1',
+  },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
